3f67a505c03402c0ad8ae637657765fcef1cd375
aki <skrbic.alexa@gmail.com>
2019-06-03 19:59:56 +0200

    etl refactored

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -24,14 +24,14 @@ class ETL():
 
     def _extract(self) -> None:
         logging.info('Loading csv files to DataFrames...')
-        self._customers: pd.DataFrame() = pd.read_csv('../../data/olist_customers_dataset.csv')
-        self._orders: pd.DataFrame() = pd.read_csv('../../data/olist_orders_dataset.csv')
-        self._order_payments: pd.DataFrame() = pd.read_csv('../../data/olist_order_payments_dataset.csv')
-        self._order_reviews: pd.DataFrame() = pd.read_csv('../../data/olist_order_reviews_dataset.csv')
-        self._order_items: pd.DataFrame() = pd.read_csv('../../data/olist_order_items_dataset.csv')
-        self._products: pd.DataFrame = pd.read_csv('../../data/olist_products_dataset.csv')
-        self._product_category_translation: pd.DataFrame = pd.read_csv('../../data/product_category_name_translation.csv')
-        self._sellers: pd.DataFrame = pd.read_csv('../../data/olist_sellers_dataset.csv')
+        self._customers = pd.read_csv('../../data/olist_customers_dataset.csv')
+        self._orders = pd.read_csv('../../data/olist_orders_dataset.csv')
+        self._order_payments = pd.read_csv('../../data/olist_order_payments_dataset.csv')
+        self._order_reviews = pd.read_csv('../../data/olist_order_reviews_dataset.csv')
+        self._order_items = pd.read_csv('../../data/olist_order_items_dataset.csv')
+        self._products = pd.read_csv('../../data/olist_products_dataset.csv')
+        self._product_category_translation = pd.read_csv('../../data/product_category_name_translation.csv')
+        self._sellers = pd.read_csv('../../data/olist_sellers_dataset.csv')
 
     def _transform(self) -> None:
         self._clean_dataframes()
@@ -50,8 +50,8 @@ class ETL():
  
     def _join_tables(self) -> None:
         logging.info('Merging DataFrames...')
-        customer_order: pd.DataFrame = self._customers.merge(self._orders, how='left', on='customer_id')
-        customer_order_payment: pd.DataFrame = customer_order.merge(self._order_payments, how='left', on='order_id')
+        customer_order = self._customers.merge(self._orders, how='left', on='customer_id')
+        customer_order_payment = customer_order.merge(self._order_payments, how='left', on='order_id')
         customer_order_payment_reviews = customer_order_payment.merge(self._order_reviews, how='left', on='order_id')
         customer_order_payment_reviews_items = customer_order_payment_reviews.merge(self._order_items, how='left', on='order_id')
         customer_order_payment_reviews_items_products = customer_order_payment_reviews_items.merge(self._products, how='left', on='product_id')
